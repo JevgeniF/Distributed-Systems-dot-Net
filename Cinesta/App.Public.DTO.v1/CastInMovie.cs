eafbd4ed015d38b7ea23e1904e1a5f5c81a65832
Jevgeni Fenko <jefenk@taltech.ee>
2022-05-23 00:58:19 +0300

    improving api controllers

--- Cinesta/App.Public.DTO.v1/CastInMovie.cs
+++ Cinesta/App.Public.DTO.v1/CastInMovie.cs
@@ -1,21 +1,26 @@
 ﻿using System.ComponentModel.DataAnnotations;
+using System.Runtime.Serialization;
 using System.Text.Json.Serialization;
 using Base.Domain;
 
 namespace App.Public.DTO.v1;
 
+[DataContract]
 public class CastInMovie : DomainEntityId
 {
+    [DataMember]
     public Guid CastRoleId { get; set; }
 
     [Display(ResourceType = typeof(Resources.App.Domain.Cast.CastInMovie), Name = nameof(CastRole))]
     public CastRole? CastRole { get; set; }
 
+    [DataMember]
     public Guid PersonId { get; set; }
 
     [Display(ResourceType = typeof(Resources.App.Domain.Cast.CastInMovie), Name = nameof(Persons))]
     public Person? Persons { get; set; }
 
+    [DataMember]
     public Guid MovieDetailsId { get; set; }
 
     [Display(ResourceType = typeof(Resources.App.Domain.Cast.CastInMovie), Name = nameof(MovieDetails))]
